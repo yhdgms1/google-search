dbd0c555fcd04a65aea2468ec645aab2ac15a3a1
Artem Schukin <shuckinart@yandex.ru>
2021-06-23 12:49:00 +0300

    make it 0.02 kb smaller

--- windicss.config.ts
+++ windicss.config.ts
@@ -4,6 +4,14 @@ import colors from 'windicss/colors'
 export default defineConfig({
   extract: {
     include: ['./**/*.html', './**/*.xht', './**/*.ma', './*.html',],
+    extractors: [
+      {
+        extractor: (content) => {
+          return { classes: content.match(/(?<=class:)[!@\w-]+/g) ?? [] }
+        },
+        extensions: ['xht', 'ma'],
+      },
+    ],
   },
   theme: {
     colors: {
