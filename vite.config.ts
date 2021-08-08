de7b8287638c392003e2fd61ca990fc6c81bb1cf
Artem Schukin <shuckinart@yandex.ru>
2021-08-08 13:04:24 +0300

    Increase search input's padding

--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,34 @@ import { minifyHtml } from 'vite-plugin-html'
 import { default as malinaPlugin } from 'malinajs/malina-rollup'
 import { default as windiCSSPlugin } from 'vite-plugin-windicss'
 
-const DEV = process.env.MODE === 'dev'
+export default defineConfig(({ mode }) => {
 
-export default defineConfig({
-  plugins: [
-    malinaPlugin({
-      extension: ['ma', 'xht'],
-      displayVersion: false,
-      compact: true,
-    }),
-    windiCSSPlugin({
-      scan: {
-        fileExtensions: ['html', 'ma', 'xht'],
-      },
-    }),
-    !DEV && viteSingleFile(),
-    !DEV && minifyHtml(),
-  ],
-  build: {
-    target: ['chrome64'],
-    polyfillDynamicImport: false,
-    cssCodeSplit: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js',
+  const DEV = mode === 'development'
+
+  return {
+    plugins: [
+      malinaPlugin({
+        extension: ['ma', 'xht'],
+        displayVersion: false,
+      }),
+      windiCSSPlugin({
+        scan: {
+          fileExtensions: ['html', 'ma', 'xht'],
+        },
+      }),
+      !DEV && viteSingleFile(),
+      !DEV && minifyHtml(),
+    ],
+    build: {
+      target: ['chrome64'],
+      polyfillDynamicImport: false,
+      cssCodeSplit: false,
+      rollupOptions: {
+        inlineDynamicImports: true,
+        output: {
+          manualChunks: () => 'everything.js',
+        },
       },
     },
-  },
+  }
 })
