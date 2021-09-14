ecf311d462196000f4c01e7e7af82fb6c5f147b6
Artemiy Schukin <shuckinart@yandex.ru>
2021-09-14 23:55:07 +0300

    replace big <path> with small <text>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 import { minifyHtml } from 'vite-plugin-html'
 import { default as malinaPlugin } from 'malinajs/malina-rollup'
 import { default as windiCSSPlugin } from 'vite-plugin-windicss'
+import { default as inspectPlugin } from 'vite-plugin-inspect'
 
 export default defineConfig(({ mode }) => {
 
@@ -21,6 +22,7 @@ export default defineConfig(({ mode }) => {
       }),
       !DEV && viteSingleFile(),
       !DEV && minifyHtml(),
+      inspectPlugin() 
     ],
     build: {
       target: ['chrome64'],
